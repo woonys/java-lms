9dc27d1cf800b04086a8f4f5c6b006fea87c354c
woony-kim <woony.kim@balancehero.com>
2023-06-26 00:28:24 +0900

    feat: SessionUser 추가:

---
+++ src/main/java/nextstep/courses/domain/SessionUser.java
@@ -0,0 +1,10 @@
+package nextstep.courses.domain;
+
+import java.time.LocalDateTime;
+
+public class SessionUser {
+    private Long id;
+    private Long sessionId;
+    private Long userId;
+    private LocalDateTime createdAt;
+}
